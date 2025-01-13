7fa874681e0a830479bc6c061298241dea97e081
Preflower <preflower.lin@gmail.com>
2025-01-13 12:25:34 +0800

    fix(torch): fix open status inconsist error, replace `switchTorch` with `setOpen` for DDV idea

--- packages/react-barcode-scanner/src/hooks/use-torch.tsx
+++ packages/react-barcode-scanner/src/hooks/use-torch.tsx
@@ -1,8 +1,33 @@
-import { useState, useMemo, useEffect } from 'react'
+import { useState, useMemo, useEffect, useCallback } from 'react'
 import { useStreamState } from './use-stream-state'
+import { createAtom, useAtom } from './use-atom'
 
-export function useTorch (open = false): [boolean, boolean, () => Promise<void>] {
-  const [isOpen, setOpen] = useState(open)
+const torchAtom = createAtom()
+
+/**
+ * Control torch of camera
+ * @param {boolean} open Torch is open by default
+ * @returns {Array} [isSupportTorch, isOpen, switchTorch, setOpen]
+ *   - isSupportTorch: Whether device supports torch
+ *   - isOpen: Whether torch is open
+ *   - switchTorch: Switch torch
+ *   - setOpen: Set open state of torch
+ *
+ * @example
+ * import React from 'react'
+ * import { useTorch } from 'react-barcode-scanner'
+ *
+ * export default () => {
+ *   const [isSupportTorch, isOpen,, setOpen] = useTorch()
+ *   return (
+ *     <div style={{ width: '100%', height: '360px' }}>
+ *       <button onClick={() => setOpen(!isOpen)}>Switch Torch</button>
+ *     </div>
+ *   )
+ * }
+ */
+export function useTorch (open = false): [boolean, boolean, () => Promise<void>, (newState: boolean) => void] {
+  const [isOpen, setOpen] = useAtom(torchAtom, open)
   const [isSupportTorch, setSupport] = useState(false)
   const [stream] = useStreamState()
   const track = useMemo(() => {
@@ -12,29 +37,40 @@ export function useTorch (open = false): [boolean, boolean, () => Promise<void>]
   useEffect(() => {
     if (track == null) return
     const capabilities = track.getCapabilities()
-
     if (capabilities.torch !== undefined) {
       setSupport(true)
     }
   }, [track])
 
-  const switchTorch = async (): Promise<void> => {
+  const setTorch = useCallback(async (torch: boolean) => {
     try {
-      if (!isSupportTorch) {
-        throw new Error(`[react-barcode-scanner]: 
-          device does not support torch capability
-        `)
-      }
-      await track!.applyConstraints({
+      if (!isSupportTorch) return
+      await track?.applyConstraints({
         advanced: [{
-          torch: !isOpen
+          torch
         }]
       })
-      setOpen(!isOpen)
     } catch (e) {
       console.warn(e)
     }
+  }, [track, isSupportTorch])
+
+  useEffect(() => {
+    setTorch(isOpen)
+  }, [isOpen, setTorch])
+
+  /**
+   * @deprecated use `setOpen` to instead of `switchTorch`
+   */
+  const switchTorch = async (): Promise<void> => {
+    if (!isSupportTorch) {
+      throw new Error('[react-barcode-scanner]: device does not support torch capability')
+    }
+    if (track == null) {
+      throw new Error('[react-barcode-scanner]: unable to capture the video stream')
+    }
+    await setTorch(!isOpen)
   }
 
-  return [isSupportTorch, isOpen, switchTorch]
+  return [isSupportTorch, isOpen, switchTorch, setOpen]
 }
